ca8019b3cf6bc59a6eb7a8493941dfcbeea0ba92
Oleg Barenboim <chessbyte@gmail.com>
2014-03-06 23:57:09 -0500

    RubyBugzilla::Bug learned to normalize attributes

--- lib/ruby_bugzilla/bug.rb
+++ lib/ruby_bugzilla/bug.rb
@@ -14,26 +14,41 @@ module RubyBugzilla
     def method_missing(meth, *args, &block)
       attribute_name = meth.to_s
       if attribute_names.include?(attribute_name)
-        raw_data[attribute_name]
+        attributes[attribute_name]
       else
         super
       end
     end
 
     def comments
-      @comments ||= begin
-        raw_data['comments'].collect { |hash| BugComment.new(hash) }
-      end
+      @comments ||= attributes['comments'].sort_by(&:count).collect { |hash| BugComment.new(hash) }
     end
 
     def attribute_names
-      @attribute_names ||= raw_data.keys.sort
+      @attribute_names ||= attributes.keys.sort
     end
 
     private
 
+    def normalize_timestamp(timestamp)
+      timestamp.respond_to?(:to_time) ? timestamp.to_time : nil
+    end
+
     def raw_data
       @raw_data ||= service.xmlrpc_bug_query(@id).first
+    end
+
+    def process_attributes(hash)
+      hash['created_on']   = normalize_timestamp(hash.delete('creation_time'))
+      hash['updated_on']   = normalize_timestamp(hash.delete('last_change_time'))
+      hash['created_by']   = hash.delete('creator')
+      hash['duplicate_id'] = hash.delete('dupe_of')
+      hash.delete_if { |k,v| v.nil? }
+      hash
+    end
+
+    def attributes
+      @attributes ||= process_attributes(raw_data)
     end
   end
 end
