436771e466dc1fdad605be0aac14998d98a3617f
Nick LaMuro <nicklamuro@gmail.com>
2018-10-24 20:13:32 -0500

    Use `:expect` syntax instead of `:should`

--- spec/active_bugzilla_spec.rb
+++ spec/active_bugzilla_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe ActiveBugzilla do
   it "::VERSION" do
-    described_class::VERSION.should be_kind_of(String)
+    expect(described_class::VERSION).to be_kind_of(String)
   end
 end
